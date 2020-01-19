1c0f3932fdfb0b08745c164a56a89a4c48b01c35
minaam.fasihi <minaam.fasihi@gmail.com>
2020-01-19 20:00:05 +0500

    Updates

---
+++ precache-manifest.ee66c8605c2fb78e8b9d642bd4cb6665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a240b2fada49f10848e661d2e701a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c9567afef53266cfc22",
+    "url": "/monsters-rolodex/static/css/main.f223f02b.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c9567afef53266cfc22",
+    "url": "/monsters-rolodex/static/js/main.faed11a0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
